1245fbe165b71a94e91d29b58466cb1fe2ea76e7
Chidi Nkwocha <profchydon@gmail.com>
2019-09-23 09:48:39 +0100

    Add gif cards

---
+++ database/migrations/2019_08_25_225612_create_gifts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gifts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('beneficiary')->unsigned();
+            $table->integer('benefactor')->unsigned()->nullable();
+            $table->string('benefactor_name');
+            $table->integer('amount');
+            $table->string('reference_id')->unique();
+            $table->foreign('beneficiary')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gifts');
+    }
+}
